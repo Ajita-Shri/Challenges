f135e081b030818dfb967adc9945e02482f61975
Ajita-Shri <61915073+Ajita-Shri@users.noreply.github.com>
2022-08-05 09:21:40 +0530

    Delete Linux Challenege directory

--- Linux Challenege/question2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cat ../Datafile/opra_example_regression.log | grep -B 2 -A 48 'Type: Trade' | while read line; 
-do grep 'wTradePrice\|wTradeVolume\|wIssueSymbol'>>raw_database;
-done 
-
-echo -e "Symbol, Trade Price, Trade Volume" >database
-#formatting that database file
-sed 's/[^ ]*  //' raw_database | awk -F'|' '{print $NF}' | awk '{ORS = (NR%3? ",":RS)}1' >>database
-

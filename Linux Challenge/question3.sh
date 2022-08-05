3c86e38f9284b31467bd8400c3ff41db2c945836
Ajita-Shri <61915073+Ajita-Shri@users.noreply.github.com>
2022-08-05 09:21:27 +0530

    Delete Linux Challenge directory

--- Linux Challenge/question3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat ../Datafile/WindowsEventLog.csv | grep "error:\|ERROR:\|Error:" > Error_file
-
-cat ../Datafile/WindowsEventLog.csv | grep "warning:\|WARNING:\|Warning:" > Warning_file
-
-cat Warning_file| awk -F',' '{print $3}'> source
-echo "The two applications that need attention are"
-sed s/' '/\\n/g source | sort | uniq -c | sort -nr | head -3 | awk '{print $2}' 
